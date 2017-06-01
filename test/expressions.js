e467a9bab37bdc721c00b69ec20ec912693b8f79
Roie Kossover <roie.kossover@gmail.com>
2017-06-01 11:06:11 +0300

    Adding order in sequences

---
+++ test/expressions.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var oil = require('../lib/oil.js');
+
+describe('Expressions', function () {
+    describe('Sequences', function () {
+        it('sequence selector', function () {
+            var result = oil.parse('states[x ? x.id == "id1" -> x]');
+            assert.equal(result.length, 1);
+            var expression = result[0];
+            assert.equal(expression["!exp"], "s");
+            assert.equal(expression.key, "x");
+            assert.equal(expression.sequence["!exp"], "i");
+            assert.equal(expression.sequence.identifier, "states");
+            assert.equal(expression.condition["!exp"], "b");
+            assert.equal(expression.selection["!exp"], "i");
+
+        });
+
+        it('ordered sequence', function () {
+            var result = oil.parse('states[x ~ -x.time]');
+            assert.equal(result.length, 1);
+            var expression = result[0];
+            assert.equal(expression["!exp"], "s");
+            assert.equal(expression.key, "x");
+            assert.equal(expression.sequence["!exp"], "i");
+            assert.equal(expression.sequence.identifier, "states");
+            assert.equal(expression.condition["!exp"], "b");
+            assert.equal(expression.selection["!exp"], "i");
+
+        });
+    });
+});
